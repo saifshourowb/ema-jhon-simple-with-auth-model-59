92389653b4eb61864768a6885cd6915c527a76ed
saif shourowb <saifshourov82@gmail.com>
2022-04-10 00:58:40 +0600

    shs

--- src/auth-steps.js
+++ src/auth-steps.js
@@ -1,4 +1,18 @@
 /*
-1.
+1. craete anew project console.google.com
+2. npm install
+3. create firebase.init.js and import getAuth to export auth
+4. firevbase setting > go to authintication >enable email and password auth
+5.
+
+*/
+
+
+/*
+* hosting steps:
+1. npm install -g firebase-tools
+2. firebase login
+3. firebase init
+4. firebase diploy
 
 */
